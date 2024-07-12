3004e908eb2197b7a9be538fce43dbf33cebccec
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-11 20:04:27 -0500

    refactor: class initialize to function properly

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,10 +1,13 @@
 class Registrant
     attr_reader :name, :age, :permit, :license_data
 
-    def initialize(profile)
-      @name = profile[:name]
-      @age = profile[:age]
-      @permit = profile[:permit]
-      @license_data = profile[:license_data]
+    def initialize(name, age, permit = false)
+      @name = name
+      @age = age
+      @permit = permit
+      @license_data = {written: false, license: false, 
+        renewed: false}
     end
+
+
 end
