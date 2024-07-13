55dc0d0ef66875c730ca8d9cadf0b10fc08d8afb
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-13 14:41:45 -0500

    feat: def register_vehicles, update_plate_type_and_fees, and set_registration_date

--- lib/facility.rb
+++ lib/facility.rb
@@ -15,6 +15,33 @@ class Facility
     @services << service
   end
 
+  def register_vehicle(vehicle)
+    return unless @services.include?('Vehicle Registration')
+    return unless vehicle.is_a?(Vehicle)
+
+    update_plate_type_and_fees(vehicle)
+    set_registration_date(vehicle)
+    @registered_vehicles << vehicle
+  end
+
+  private
+
+  def update_plate_type_and_fees(vehicle)
+    if vehicle.antique?
+      vehicle.plate_type = :antique
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle?
+      vehicle.plate_type = :ev
+      @collected_fees += 200
+    else
+      vehicle.plate_type = :regular
+      @collected_fees +=100
+    end
+  end
+
+  def set_registration_date(vehicle)
+    vehicle.registration_date = Date.today
+  end
 
 
 end
