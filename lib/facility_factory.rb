661b5567f7249596a97401dbb948dff4fb001fa0
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-14 19:52:40 -0500

    feat: added Missouri DMV to def create_office(info)

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,8 +3,8 @@ class FacilityFactory
     def create_office(info)
         info.map do |office_info|
             Facility.new({
-            name: office_info[:dmv_office] || office_info[:office_name],
-            address: office_info[:address_li] || office_info[:street_address_line_1],
+            name: office_info[:dmv_office] || office_info[:office_name] || office_info[:name],
+            address: office_info[:address_li] || office_info[:street_address_line_1] || office_info[:address1],
             phone: office_info[:phone] || office_info[:public_phone_number],
             services: office_info[:services_p]
             })
@@ -18,3 +18,7 @@ end
 #   :public_phone_number=>"7184774820",
 #   :street_address_line_1=>"1815 E JERICHO TURNPIKE",
 #   
+#  MO
+# :name=>"FERGUSON-OFFICE CLOSED UNTIL FURTHER NOTICE",
+# :address1=>"10425 WEST FLORISSANT",
+#  :phone=>"(314) 733-5316",
