b8675a672d28f65306daa7c0a1096764c34c52b5
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-14 19:27:12 -0500

    feat: offices from New York DMV locations

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -14,6 +14,20 @@ RSpec.describe FacilityFactory do
         expect(co_offices.first.name).to eq('DMV Tremont Branch')
         expect(co_offices.first.address).to eq('2855 Tremont Place')
         expect(co_offices.first.phone).to eq('(720) 865-4600')
-        # expect(co_offices.services).to eq(['vehicle titles, registration, renewals;  VIN inspections'])
+        expect(co_offices.first.services).to eq('vehicle titles, registration, renewals;  VIN inspections')
+    end
+
+    it 'creates office from New Year DMV office locations' do
+        factory = FacilityFactory.new
+        ny_factory = DmvDataService.new.ny_dmv_office_locations
+        ny_offices = factory.create_office(ny_factory)
+
+        expect(ny_offices).to be_an(Array)
+        expect(ny_offices.first).to be_a(Facility)
+        
+        expect(ny_offices.first.name).to eq('HUNTINGTON')
+        expect(ny_offices.first.address).to eq('1815 E JERICHO TURNPIKE')
+        expect(ny_offices.first.phone).to eq('7184774820')
+        # New York doesn't list their services
     end
 end
