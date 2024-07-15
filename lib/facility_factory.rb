fbb3bc461444ba6a47c0fffb569bea5630b24e48
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-14 19:28:10 -0500

    feat: added NY to def create_office method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,17 +3,18 @@ class FacilityFactory
     def create_office(info)
         info.map do |office_info|
             Facility.new({
-            name: office_info[:dmv_office],
-            address: office_info[:address_li],
-            address_1: office_info[:address_1],
-            city: office_info[:city],
-            state: office_info[:state],
-            zip: office_info[:zip],
-            phone: office_info[:phone],
-            # services: office_info[:services_p]
+            name: office_info[:dmv_office] || office_info[:office_name],
+            address: office_info[:address_li] || office_info[:street_address_line_1],
+            phone: office_info[:phone] || office_info[:public_phone_number],
+            services: office_info[:services_p]
             })
         end
 
     end
 
 end
+#  NY's info:
+# > [{:office_name=>"HUNTINGTON",
+#   :public_phone_number=>"7184774820",
+#   :street_address_line_1=>"1815 E JERICHO TURNPIKE",
+#   
